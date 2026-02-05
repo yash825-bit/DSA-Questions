bc5ccdd4ad483224d88c937408fc95174e38d50e
yash825-bit <goyal.yash18335@gmail.com>
2026-02-05 09:34:26 +0530

    back with dsa

--- BasicMaths/HCF.cpp
+++ BasicMaths/HCF.cpp
@@ -2,14 +2,8 @@
 #include <iostream>
 using namespace std;
 
-void pattern18(int n){
-    for(int i = 0; i<n; i++){
-        for(int j = n-i; j<=n; j++){
-            cout<<"*"<<" ";
-        }
-        cout<<endl;
-    }
-}
+
+
 int main(){
     int t;
     cin >> t;
@@ -17,7 +11,6 @@ int main(){
         int n;
         cin >> n;
         // function call here with input n->
-        pattern18(n);
     }
     return 0;
 }
